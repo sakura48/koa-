d68b3efd8879a7f352ed05220d47eb662b6d7ce7
chen_you <chin_you@163.com>
2018-01-19 17:35:54 +0800

    增加了静态文件打包压缩功能

---
+++ config/webpack.config.js
@@ -0,0 +1,32 @@
+const fs = require('fs')
+const path = require('path')
+const webpack = require('webpack')
+
+function ent () {
+  let ent = {}
+  let file = fs.readdirSync(path.resolve(__dirname, '../static/js/'))
+  if (file.length !== 0) {
+    file.forEach(val => {
+      if (val.endsWith('min.js')) {
+        let filename = path.basename(val, '.min.js')
+        ent[filename] = './static/js/' + val
+      }
+    })
+  }
+  return ent
+}
+
+module.exports = {
+  entry: ent(),
+  output: {
+    filename: '[name].min.js'
+  },
+  // plugins: [
+  //   new webpack.ProvidePlugin({
+  //     $: 'jquery'
+  //   })
+  // ],
+  stats: {
+    colors: true
+  }
+}
--- controller/index.js
+++ controller/index.js
@@ -1,12 +1,50 @@
 const path = require('path')
+const fs = require('fs')
+const WXBizDataCrypt = require('../lib/WXBizDataCrypt')
+
 let index = async (ctx, next) => {
+  //  console.log(ctx.req)
+  var appId = 'wx4f4bc4dec97d474b'
+  var sessionKey = 'tiihtNczf5v6AKRyjwEUhQ=='
+  var encryptedData =
+    'CiyLU1Aw2KjvrjMdj8YKliAjtP4gsMZM' +
+    'QmRzooG2xrDcvSnxIMXFufNstNGTyaGS' +
+    '9uT5geRa0W4oTOb1WT7fJlAC+oNPdbB+' +
+    '3hVbJSRgv+4lGOETKUQz6OYStslQ142d' +
+    'NCuabNPGBzlooOmB231qMM85d2/fV6Ch' +
+    'evvXvQP8Hkue1poOFtnEtpyxVLW1zAo6' +
+    '/1Xx1COxFvrc2d7UL/lmHInNlxuacJXw' +
+    'u0fjpXfz/YqYzBIBzD6WUfTIF9GRHpOn' +
+    '/Hz7saL8xz+W//FRAUid1OksQaQx4CMs' +
+    '8LOddcQhULW4ucetDf96JcR3g0gfRK4P' +
+    'C7E/r7Z6xNrXd2UIeorGj5Ef7b1pJAYB' +
+    '6Y5anaHqZ9J6nKEBvB4DnNLIVWSgARns' +
+    '/8wR2SiRS7MNACwTyrGvt9ts8p12PKFd' +
+    'lqYTopNHR1Vf7XjfhQlVsAJdNiKdYmYV' +
+    'oKlaRv85IfVunYzO0IKXsyl7JCUjCpoG' +
+    '20f0a04COwfneQAGGwd5oa+T8yO5hzuy' +
+    'Db/XcxxmK01EpqOyuxINew=='
+  var iv = 'r7BXXKkLb8qrSNn05n0qiA=='
+
+  var pc = new WXBizDataCrypt()
+  pc.appId = appId
+  var data = pc.decryptData(sessionKey, encryptedData, iv)
+
+  console.log('解密后 data: ', data)
   ctx.render('hello.html', {'name': 'f1321'})
 }
 
 let signin = async (ctx, next) => {
   // let oldpath = ctx.request.files.file.path
-  console.log(path.basename('../lib/file.js'))
-  ctx.render('hello.html', {'name': 'da'})
+  console.log(ctx.request.body)
+  let pc = new WXBizDataCrypt()
+  let session = await pc.getSession(ctx.request.body.code)
+  console.log(session)
+  let data = pc.decryptData(session.session_key, ctx.request.body.encryptedData, ctx.request.body.iv)
+  console.log(data)
+  //  ctx.response.type = 'json'
+  ctx.response.body = data
+  //  ctx.render('hello.html', {'name': 'da'})
 }
 
 module.exports = {
